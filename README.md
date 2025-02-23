0ac90f141b6b05261259a355113a9329c3a4ff12
Hajar Razip <mshajarrazip@gmail.com>
2025-02-23 15:40:50 +0800

    Update files

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # kafka-python
-Following kafka-python quickstart tutorial at https://developer.confluent.io/get-started/python/ ~
+
+This repo contains a docker compose setup for a local kafka cluster,
+and some artifacts for following the tutorials at
+[Getting Started with Apache Kafka and Python](https://developer.confluent.io/get-started/python).
+
+# Starting Up
+
+1. This project runs on either Linux or WSL 2.
+1. Initialize the environment & install the dependencies:
+    ```
+    pip install confluent-kafka
+    ```
+1. Run `docker compose up` to start the kafka clusters.
+1. Run [consumer.py](consumer.py) ( monitor the output ):
+    ```
+    python consumer.py
+    ```
+1. Run producer.py:
+    ```
+    python producer.py
+    ```
+
+## References
+
+1. [Getting Started with Apache Kafka and Python](https://developer.confluent.io/get-started/python).
+1. [Quick Start for Confluent Platform](https://docs.confluent.io/platform/current/get-started/platform-quickstart.html#quickstart)
---
+++ consumer.py
@@ -0,0 +1,42 @@
+from random import choice
+from confluent_kafka import Consumer
+
+if __name__ == "__main__":
+
+    config = {
+        
+        
+        'bootstrap.servers': 'localhost:9092',
+        'security.protocol': 'PLAINTEXT',
+
+        # User specific properties that you must set
+        'group.id': 'kafka-python',
+        'auto.offset.reset': 'earliest',
+
+    }
+
+    consumer = Consumer(config)
+
+    topic = "purchases"
+    consumer.subscribe([topic])
+
+    # Poll for new messages from Kafka and print them.
+    try:
+        while True:
+            msg = consumer.poll(1.0)
+            if msg is None:
+                # Initial message consumption may take up to
+                # `session.timeout.ms` for the consumer group to
+                # rebalance and start consuming
+                print("Waiting...")
+            elif msg.error():
+                print("ERROR: %s".format(msg.error()))
+            else:
+                # Extract the (optional) key and value, and print.
+                print("Consumed event from topic {topic}: key = {key:12} value = {value:12}".format(
+                    topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Leave group and commit final offsets
+        consumer.close()
